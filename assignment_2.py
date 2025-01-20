5f77841664ec6cdfd39adaa46be431b5dbafcc93
Lars Kälber <ac144316@uni-stuttgart.de>
2025-01-20 17:44:48 +0100

    added empty functions

---
+++ assignment_2.py
@@ -0,0 +1,99 @@
+# %%
+# write a function that appends an element to a list
+def get_list_with_appended_element(list, element):
+    '''
+    returns a list to whom an element is appended
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that appends the elements of a list to another list
+def get_list_with_appended_list(list1, list2):
+    '''
+    returns a list to whom the elements of another list are appended
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the length of a list
+def get_length_of_list(list):
+    '''
+    returns the length of a list
+    '''
+    
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the square numbers of a range from 0 to n with list comprehension
+def get_squares_of_range(n):
+    '''
+    returns the square numbers of a range from 0 to n
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns an array of arrays with the square of numbers from range 0 to m as first element and the sine of numbers from range 0 to n as second element
+# it should look like [[m0**2, sin(n0)], [m0**2, sin(n1)], [m0**2, sin(n2)], [m1**2, sin(n0)], [m1**2, sin(n1)],...]
+def get_squares_and_sine_of_ranges(m, n):
+    '''
+    returns an array of arrays with the square of numbers from range 0 to n as first element and the sine of numbers as second element
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that adds an value with a key to a dictionary
+def add_value_to_dict(dict, key, value):
+    '''
+    returns a dictionary to whom a value is added with a key
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the value of a key in a dictionary
+def get_value_from_dict(dict, key):
+    '''
+    returns the value of a key in a dictionary
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the keys of a dictionary
+def get_keys_from_dict(dict):
+    '''
+    returns the keys of a dictionary
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
+
+# %%
+# write a function that returns the values of a dictionary
+def get_values_from_dict(dict):
+    '''
+    returns the values of a dictionary
+    '''
+
+    # TODO: YOUR CODE HERE
+
+    return result
